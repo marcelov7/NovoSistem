763d9610266a3d864112f00d5c2e40b6ff616d6c
NovoSistem Dev <dev@novosistem.com>
2025-06-05 11:33:19 -0300

    fix: Configurar SSL mode diretamente no config database.php

--- config/database.php
+++ config/database.php
@@ -94,7 +94,10 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => env('DB_SSLMODE', 'disable'),
+            'options' => [
+                PDO::PGSQL_ATTR_DISABLE_PREPARES => true,
+            ],
         ],
 
         'sqlsrv' => [
